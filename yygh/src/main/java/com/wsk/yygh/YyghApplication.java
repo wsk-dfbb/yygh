2a6630ba86eab9c17e4c24843f103f8e91f9bac5
config <1582508291@qq.com>
2021-03-16 11:01:11 +0800

    parent

---
+++ yygh/src/main/java/com/wsk/yygh/YyghApplication.java
@@ -0,0 +1,13 @@
+package com.wsk.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghApplication.class, args);
+    }
+
+}
---
+++ yygh/src/test/java/com/wsk/yygh/YyghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsk.yygh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
